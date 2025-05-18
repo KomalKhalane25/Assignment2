7cdfdf1ea51acd0059aafde7956a374364e836ab
Komal <Khalanekomal2001@gmail.com>
2025-05-18 16:49:31 +0530

    MyFirstAngularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'Assignment2';
+  number1: number = 0; 
+  number2: number = 0;    
 }
